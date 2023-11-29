b4a201e62251f64105677f6dfdebbb61b3de1d21
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-11-29 13:11:26 +0100

    created by chatGPT
    https://chat.openai.com/share/963b7290-e851-4155-bb94-1f30dd74422d

---
+++ static-site-generator.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import errno
+from datetime import datetime
+
+def main(template_path, output_dir):
+    # Read the template file
+    with open(template_path, 'r') as template_file:
+        template_content = template_file.read()
+
+    # Iterate through content files
+    for subdir, _, files in os.walk(output_dir):
+        for file in files:
+            if file.endswith(".content"):
+                content_file_path = os.path.join(subdir, file)
+
+                # Extract date information from the filename
+                date_str = os.path.splitext(file)[0]
+                date = datetime.strptime(date_str, "%d.%m").replace(year=int(os.path.basename(subdir)), hour=0, minute=0, second=0)
+
+                # Create the result string
+                with open(content_file_path, 'r') as content_file:
+                    result = template_content.replace('<div class="content"></div>', f'<div class="content">{content_file.read()}</div>')
+
+                # Print date, hyphens, and result
+                print(f"{date.strftime('%Y-%m-%d')}\n{'-' * 80}\n{result}\n{'-' * 80}")
+
+                # Create or warn about existing HTML file
+                html_output_path = os.path.join(output_dir, date.strftime('%Y/%m/%d.html'))
+                if not os.path.exists(html_output_path):
+                    try:
+                        os.makedirs(os.path.dirname(html_output_path))
+                    except OSError as e:
+                        if e.errno != errno.EEXIST:
+                            raise
+                    with open(html_output_path, 'w') as html_file:
+                        html_file.write(result)
+                else:
+                    print("\033[91mWarning: The file {} already exists.\033[0m".format(html_output_path), file=sys.stderr)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py template_path output_dir")
+        sys.exit(1)
+
+    template_path = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    if not os.path.isfile(template_path):
+        print(f"Error: Template file {template_path} not found.")
+        sys.exit(1)
+
+    if not os.path.isdir(output_dir):
+        print(f"Error: Output directory {output_dir} not found.")
+        sys.exit(1)
+
+    main(template_path, output_dir)
